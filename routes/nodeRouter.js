be5050d39fe28bdad7f46b3f5cf707e64cb2d376
cecil john tantay <cjbtantay@gmail.com>
2020-01-07 11:39:02 -0800

    change stuff

--- routes/nodeRouter.js
+++ routes/nodeRouter.js
@@ -42,19 +42,67 @@ router.post('/', async (req, res) => {
   ) {
     res.status(422).json({ message: 'missing fields required' });
   }
+
+  const exitObj = {};
+  exits.forEach(item => (exitObj[item] = '?'));
+
   try {
     const data = await db('node').insert({
       room_id,
       title,
       description,
       coordinates: JSON.stringify(coordinates),
-      exits: JSON.stringify(exits),
+      exits: exitObj,
       cooldown,
       errors: JSON.stringify(errors),
       messages: JSON.stringify(messages)
     });
     console.log(data);
     res.status(201).json({ message: 'node has been created' });
+  } catch ({ message }) {
+    console.error(message);
+    res.status(500).json({ message });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const {
+    room_id,
+    title,
+    description,
+    coordinates,
+    exits,
+    cooldown,
+    errors,
+    messages
+  } = req.body;
+  if (
+    !room_id ||
+    !title ||
+    !description ||
+    !coordinates ||
+    !exits ||
+    !cooldown ||
+    !errors ||
+    !messages
+  ) {
+    res.status(422).json({ message: 'missing fields required' });
+  }
+  try {
+    await db('node')
+      .where({ room_id: id })
+      .update({
+        room_id,
+        title,
+        description,
+        coordinates: JSON.stringify(coordinates),
+        exits: JSON.stringify(exits),
+        cooldown,
+        errors: JSON.stringify(errors),
+        messages: JSON.stringify(messages)
+      });
+    res.status(200).json({ message: 'node changed' });
   } catch ({ message }) {
     console.error(message);
     res.status(500).json({ message });
